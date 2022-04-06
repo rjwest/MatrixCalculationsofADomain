786574495096551b706b5939103ede5879141832
rjwest <39276715+rjwest@users.noreply.github.com>
2022-04-06 03:32:23 -0400

    Initial commit
    Initial commit containing the work so far in taking a list of coeffs and applying gradient ascent to generate matricies of significant points of orbits

---
+++ tangent_parameterization.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 24 20:48:14 2022
+
+@author: Robert
+"""
+
+import scipy.integrate as integrate
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+                
+def calcXAnalytic(theta, pairs):
+    a_sum = 0
+    b_sum = 0
+    for k in range(2, len(pairs), 1):
+        a,b = pairs[k].split(',')
+        a = float(a)
+        b = float(b)
+        a_sum += (a/2) * (((1 - (np.cos((k+1)*theta))) / (k + 1)) + ((1 - (np.cos((k - 1)*theta))) / (k - 1)))
+        b_sum += (b/2) * (((np.sin((k+1)*theta)) / (k + 1)) + ((np.sin((k - 1)*theta)) / (k - 1)))
+    
+    a,b = pairs[0].split(',')
+    b = float(b)
+    return a_sum + b_sum + (b * np.sin(theta))
+
+def calcYAnalytic(theta, pairs):
+    a_sum = 0
+    b_sum = 0
+    for k in range(2, len(pairs), 1):
+        a,b = pairs[k].split(',')
+        a = float(a)
+        b = float(b)
+        a_sum += (a/2) * (-(((np.sin((k+1)*theta))) / (k + 1)) + (((np.sin((k - 1)*theta))) / (k - 1)))
+        b_sum += (b/2) * (((1 - (np.cos((k+1)*theta))) / (k + 1)) + (((np.cos((k - 1)*theta)) - 1) / (k - 1)))
+    
+    a,b = pairs[0].split(',')
+    b = float(b)
+    return a_sum + b_sum + (b * (1 - np.cos(theta)))
+
+
+
+
+
+def plotPoints(fname):
+
+    x_list = []
+    y_list = []
+    pairs = []
+
+    with open(fname) as fp:
+        for line in fp:
+            line = line.strip()
+            pairs = line.split('~')
+            
+    ep = math.pi/100
+
+    #adding epsilon will add 1 last point which ends up near the beginning point of graph        
+    for theta in np.arange(0,(2*math.pi)+ep,ep): 
+        x = calcXAnalytic(theta, fname, pairs)
+        y = calcYAnalytic(theta, fname, pairs)
+        x_list += [x]
+        y_list += [y]
+      
+    print(len(x_list))
+    print(f'x list: {x_list}')
+    print(f'y list: {y_list}')   
+    
+    
+    plt.axes().set_aspect('equal')
+    #plt.scatter(x_list,y_list)
+    plt.plot(x_list,y_list)
+    
+def getPoints(pairs, theta_list): 
+    
+    x_list = []
+    y_list = []
+                  
+    for theta in theta_list: 
+        x = calcXAnalytic(theta, pairs)
+        y = calcYAnalytic(theta, pairs)
+        x_list += [x]
+        y_list += [y]
+    
+    return(x_list, y_list)
+
+
+if __name__ == '__main__':
+    fname_list = 'coeff1.txt'
+    plotPoints(fname_list)       
+
+
+    
---
+++ tying_together.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 19:14:43 2022
+
+@author: Robert
+"""
+
+import scipy.integrate as integrate
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import multiprocessing
+
+from gradient_descent_model import gradient_ascent
+from tangent_parameterization import getPoints
+
+
+THREADS = 20
+
+def _l_of_thetas(x_list, y_list, q):
+    summation = 0
+    for j in range(q):
+        if j + 1 == q:
+            j_plus_1 = 0
+        else:
+            j_plus_1 = j + 1
+            
+        summation += (math.sqrt((x_list[j_plus_1] - x_list[j])**2 + (y_list[j_plus_1] - y_list[j])**2))
+    return summation
+
+def _rho_of_theta(theta, pairs):
+    bs = []
+    for pair in pairs:
+        a,b = pair.split(',')
+        a = float(a)
+        bs += [float(b)]
+    summation = 0
+    for k in range(len(pairs)):
+        summation += bs[k] * np.cos(k*theta)
+    return summation
+
+def _z_of_theta(theta, pairs):
+    return integrate.quad(lambda t,pairs: _rho_of_theta(t, pairs)**(1/3), 0, theta, args=(pairs))[0]
+
+def _e_n(theta, pairs, k, c): 
+    lp = (2*math.pi)/c
+    
+    return np.cos(k * (lp * _z_of_theta(theta, pairs)))    
+
+def _fetch_val_matrix_Anq(gradient_thetas, k, c, q, pairs):
+    summation = 0
+    for j in range(q):
+        if j+1 == q:
+            j1 = 0
+        else:
+            j1 = j+1
+            
+        x_list,y_list = getPoints(pairs, [gradient_thetas[j], gradient_thetas[j1]]) 
+        
+        alpha_l = np.arctan2(y_list[1] - y_list[0], x_list[1] - x_list[0])
+        
+        if alpha_l < 0:
+            alpha_l += 2*math.pi
+        
+        plt.plot(x_list,y_list)
+
+        #print(f'Argument of sine: {(alpha_l - gradient_thetas[j])/(2*math.pi)}\n j is: {j}\n q is: {q}')
+        #print(f'alpha is: {alpha_l}\n Gradient theta is: {gradient_thetas[j]}')
+        summation += _e_n(gradient_thetas[j], pairs, k, c) * np.sin(( alpha_l - gradient_thetas[j]))
+    return summation
+        
+def gen_matrix_Anq(pairs, N, x_list_domain, y_list_domain):
+    theta_list = []
+    matrix = np.array([[]])
+    non = 0
+    c = integrate.quad(lambda t,pairs: _rho_of_theta(t, pairs)**(1/3), 0, 2*math.pi, args=(pairs))[0]
+    
+    for q in range(2, (N+2), 1):
+        theta_list = []
+        
+        #Get gradient thetas for q
+        row = np.array([])
+        ep = 2*math.pi/q        
+        for theta in np.arange(0,(q*ep),ep):
+            theta_list += [theta]
+        
+        #print(q*ep)
+        #print(theta_list)
+        
+        gradient_thetas = gradient_ascent(theta_list, pairs, 0.01)
+        #print(f'grad! {gradient_thetas}')
+
+        #Generate each index of a row in the matrix
+        for k in range(2, N+2):
+            row = np.append(row, [_fetch_val_matrix_Anq(gradient_thetas, k, c, q, pairs)])
+        if non == 0:
+            matrix = [row]
+            non = 1
+            print(f'Row {q-1} computed')
+        else:
+            matrix = np.append(matrix, [row], axis=0)
+            print(f'Row {q-1} computed')
+    
+    return matrix
+    
+if __name__ == "__main__":
+    
+    fname = 'coeff1.txt'
+    theta_list_domain = []
+    pairs = []
+    
+    
+    #BUILD THE ORIGINAL DOMAIN
+    orig_q = 201
+    orig_ep = 2*math.pi/orig_q
+    with open(fname) as fp:
+        for line in fp:
+            line = line.strip()
+            pairs = line.split('~')
+    
+    for theta in np.arange(0,(orig_q*orig_ep) + orig_ep,orig_ep):
+        theta_list_domain += [theta]
+        
+    x_list_domain, y_list_domain = getPoints(pairs, theta_list_domain)    
+
+    #plt.axes().set_aspect('equal')
+    #plt.plot(x_list_domain,y_list_domain)
+     
+    N = 200
+    matrix = gen_matrix_Anq(pairs, N, x_list_domain, y_list_domain)
+    
+    temp = np.linalg.eigvals(matrix)
+    x_list = [ele.real for ele in temp]
+    y_list = [ele.imag for ele in temp]
+    
+    max_x = max(x_list)
+    min_x = min(x_list)
+    
+    max_y = max(y_list)
+    min_y = min(y_list)
+    
+    print('MATRIX COMPUTED')
+    #print(matrix)
+    
+    for n in range(2,N):
+        #print(matrix[0:n,0:n])
+        inner_matrix = matrix[0:n,0:n]
+        eiganvals = np.linalg.eigvals(inner_matrix)
+        
+        # extract real part
+        x = [ele.real for ele in eiganvals]
+        # extract imaginary part
+        y = [ele.imag for ele in eiganvals]
+        
+        plt.figure()
+        plt.xlim(min_x - 0.5, max_x + 0.5)
+        plt.ylim(min_y - 0.5, max_y + 0.5)
+        plt.scatter(x, y)
+        
+        plt.ylabel('Imaginary')
+        plt.xlabel('Real')
+        plt.savefig(f'eigans//eigans_{n}.png')
+        plt.show()
+        
+        '''
+        #PRINTS THE MATRIX IN READABLE FORM
+        np.set_printoptions(threshold=np.inf)
+        for cell in matrix:
+            string = ''
+            for item in cell:
+                string = f'{string} {item.round(3)}'    
+            print(string)
+        '''
+        
+    plt.figure()
+    plt.xlim(min_x - 0.5, max_x + 0.5)
+    plt.ylim(min_y - 0.5, max_y + 0.5)
+    plt.scatter(x_list, y_list)
+    
+    plt.ylabel('Imaginary')
+    plt.xlabel('Real')
+    plt.savefig(f'eigans//eigans_{N}.png')
+    plt.show()
+            
+        
+            
+        #print('')
+        #print(f'eigens: {eiganvals}')
